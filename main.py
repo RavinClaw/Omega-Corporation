a51147fbc3f57d8f61615d0623b6e3513fed7f40
Ravin <87692753+RavinClaw@users.noreply.github.com>
2022-07-02 23:09:19 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import socket
+from time import sleep
+from random import randint as rd
+
+# Change this depending if your system uses "cls" or "clear"
+cls = 'clear'
+
+def Communication():
+  os.system(cls)
+  print("Loading Communication")
+  print("Loading Signals")
+  sleep(20)
